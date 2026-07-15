f15abfeb3f14cfcb510902264297be386235e9b5
arsenic3375 <denis.v.arsenev@gmail.com>
2026-07-14 23:56:16 -0400

    Added road map •⩊•

--- popup/popup.js
+++ popup/popup.js
@@ -1,51 +1,67 @@
-function rotateToMouse(card) {
-    return function(e) {
-        let bounds = card.getBoundingClientRect();
-        //mouse coordinates relative to bounds
-        let mouse = {
-            x: e.clientX - bounds.x,
-            y: e.clientY - bounds.y
-        }
+let canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-        //center coordinates relative to bounds
-        let center = {
-            x: bounds.width / 2,
-            y: bounds.height / 2
-        }
+let ctx = canvas.getContext("2d");
 
-        //poisiton vector of mouse
-        let center_mouse = {
-            x: mouse.x - center.x,
-            y: mouse.y - center.y
-        }
-
-        const distance = Math.sqrt(center_mouse.x**2 + center_mouse.y**2);
-    
-        card.querySelector('.glow').style.backgroundImage = `
-        radial-gradient(circle at ${mouse.x}px ${mouse.y}px, #ffffff55, #0000000f)`;
-
-        card.style.transform = `
-        rotate3d(${center_mouse.y}, ${-center_mouse.x}, 0, ${Math.log(distance)*5}deg)`;
-    }
+function background(color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-let card_containers = document.querySelectorAll(".card_container");
-console.log(card_containers);
+class Cat{
+    constructor(){
+        this.hunger      = 0;
+        this.fatigue     = 0;
+        this.boredom     = 0;
+        
+        this.max_hunger  = 100;
+        this.max_fatigue = 100;
+        this.max_boredom = 100;
 
-card_containers.forEach(card_container => {   
 
-    let card = card_container.querySelector('.card');
-    let handler = rotateToMouse(card);
+    }
 
-    card_container.addEventListener('mouseenter', () => {
-        document.addEventListener('mousemove', handler);
-    });
-  
+    eat() {}
+    sleep() {}
+    play() {}
+
+    draw() {}
+}
+
+/*
+Core Game Loop:
+    Cat starts of fine, as it continues living, it accumulates: hunger, fatigue, boredom
+    Feed cat to reduce hunger
+    Play with cat to reduce bordeom
     
-    card_container.addEventListener('mouseleave', () => {
-        document.removeEventListener('mousemove', handler);
-        card.style.transform = '';
-        card.querySelector('.glow').style.backgroundImage = '';
-    });
+    if cat is too hungry, it will die
+    if cat is too tired, it will die
+    if cat is too tired and not too hungry or too bored, it will sleep
     
-});
+    If cat is too hungry and you play with it, it will be angry
+    If cat is not bored and you play with it, it will ignore you
+    If cat is too tired and you play with it, it will be anoyed
+
+    if cat is too bored and not hungry and you feed it, it will knock over food
+
+    I am thinking of more behavior to add later
+
+    You can play with cat by stroking it with cursor, or holding down on mousepad to shine laser
+    Cat will be happy when you play with it
+    Cat Will be happy when you feed it
+    Cat will be happy while asleep
+
+    I need to add a bunch of canvas animations for all of these, as well as sounds of purring and such
+    I need to be able to select from a set of matching combinations that fit the current values for hunger, fatigue, boredom
+
+    EX:
+        //type examples here
+*/
+
+function update() {
+    background("rgb(255, 255, 255)");
+    requestAnimationFrame(update);
+}
+
+update();
